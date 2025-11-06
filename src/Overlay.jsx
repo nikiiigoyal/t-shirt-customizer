d7f35ee1bf840a275ed24274d4f972c8f8a04a73
Nikita Goyal <nikiiigoyal@gmail.com>
2025-11-06 14:31:45 +0530

    Modified code

---
+++ src/Overlay.jsx
@@ -0,0 +1,15 @@
+import { Logo } from "@pmndrs/branding";
+import { AiOutlineShopping } from "react-icons/ai";
+
+export function Overlay () {
+return (
+  <div>
+ <motion.header>
+  <Logo />
+  <AiOutlineShopping />
+ </motion.header>
+
+  </div>
+)
+
+}
