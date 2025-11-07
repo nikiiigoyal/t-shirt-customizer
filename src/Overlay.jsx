c35e640a8696e342f228c8b16d6320403aa3660a
Nikita Goyal <nikiiigoyal@gmail.com>
2025-11-07 12:04:53 +0530

    Add code in overlay component

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -1,15 +1,103 @@
 import { Logo } from "@pmndrs/branding";
-import { AiOutlineShopping } from "react-icons/ai";
+import { motion,AnimatePresence } from "framer-motion";
+import { AiFillCamera, AiOutlineArrowLeft, AiOutlineHighlight, AiOutlineShopping } from "react-icons/ai";
+import { useSnapshot } from "valtio";
+import { state } from "./Store";
+import { Decal } from "@react-three/drei";
 
 export function Overlay () {
+  const snap = useSnapshot(state);
+  const transition = { type: 'spring', duration: 0.8 }
+  const config = {
+    initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
+    animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
+    exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } }
+  }
 return (
-  <div>
- <motion.header>
-  <Logo />
-  <AiOutlineShopping />
+  <div style={{position: 'absolute',top:'0',left:'0',width:'100%',height:'100%'}}>
+    {/* header */}
+ <motion.header initial={{opacity: 0, y: -100}}
+ animate={{opacity:1,y:0}} transition={transition}>
+  <Logo width='40' height='40'/>
+  <motion.div animate={{ x: snap.intro ? 0 : 100, opacity: snap.intro ? 1 : 0 }} transition={transition}>
+  <AiOutlineShopping  size='3em'/>
+  </motion.div>
  </motion.header>
-
+<AnimatePresence>
+{snap.intro ? (
+  <motion.section key='main' {...config}>
+    <div className="section--container">
+      {/* seperate animation for this title */}
+      <motion.div key="title" 
+      initial={{x: 100, opacity: 0}}
+      animate={{x: 0, opacity: 1}}
+      transition={{type: 'spring',damping: 5, stiffness:40, restDelta: 0.001, duration:0.3}}
+      >
+        <h1>Let's DO IT</h1>
+      </motion.div>
+      {/* sepreate animaion for para */}
+      <div className="support--container">
+    <motion.div key='p' initial={{y: 100,opacity:0}}
+    animate={{y: 0, opacity:1}}
+    transition={{type: 'spring',damping: 7, stiffness:30, restDelta: 0.001, duration:0.6,delay:0.2,delayChildren:0.2}}
+    >
+  <p>Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong> and define your
+                    own style.</p>
+                    {/* button */}
+<button>CUSTOMIZE IT
+  <AiOutlineHighlight size='1.3em'/>
+</button>
+    </motion.div>
+      </div>
+    </div>
+  </motion.section>
+):(
+ <motion.section key='custom' {...config}>
+  <Customizer />
+ </motion.section>
+)}
+</AnimatePresence>
   </div>
 )
+}
 
+// customizer component
+function Customizer () {
+const snap = useSnapshot(state)
+return (
+  <div className="customizer">
+    <div className="color-options">
+      {/* loop thriugh each color in colors array */}
+      {snap.colors.map ((color) => (
+  <div key={color} className={`circle`} style={{background:color}} onClick={() => (state.color = color)}></div>
+
+      ))}
+    </div>
+  {/* decals picker this chnages logo */}
+  <div className="declas">
+    <div className="decals-container">
+      {snap.decals.map((decal) => (
+        <div key={decal} className={`decal`} onClick={() => (state.decal = decal)}>
+          <img src={decal + '_thumb.png'} alt="brand"></img>
+        </div>
+      ))}
+    </div>
+  </div>
+  <button className="share" style={{background: snap.color}} onClick={() => {
+    const link = document.createElement('a')
+    link.setAttribute('download','canvs.png')
+    link.setAttribute('href', document.querySelector('canvas').toDataURL('image/png').replace('image/png', 'image/octet-stream'))
+    link.click()
+  }
+  } >
+    DOWNLOAD
+    <AiFillCamera  size='1.3em'/>
+  </button>
+  {/* Go back button */}
+  <button className="exit" style={{ background: snap.color }} onClick={() => (state.intro = true)}>
+    GO BACK
+    <AiOutlineArrowLeft size='1.3em' />
+  </button>
+  </div>
+)
 }
