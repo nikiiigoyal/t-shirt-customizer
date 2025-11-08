0dcdf97f0856ee04fbc03a9edf664e9b773f08ef
Nikita Goyal <nikiiigoyal@gmail.com>
2025-11-08 20:27:34 +0530

    Update in app , store,gitignore files

--- src/Store.jsx
+++ src/Store.jsx
@@ -2,9 +2,11 @@ import { proxy } from "valtio";
 // Valtio creates a "magic" state object. When you change state.color, every component watching it automatically re-renders. It's like a global variable that triggers updates everywhere.
 
 const state = proxy({
-  intro: true,
+  intro: true, 
+  // array of available colors and logos
   colors: ['#ccc', '#EFBD4E', '#80C670', '#726DE8', '#EF674E', '#353934'],
   decals: ['react', 'three2', 'pmndrs'],
+  // array of selected color and logo
   color: '#EFBD4E',
   decal: 'three2'
 })
