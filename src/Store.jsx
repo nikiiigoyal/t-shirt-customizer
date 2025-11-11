2f215d1bedc15ac7de77f7a674e40c39b2c4fcde
Nikita Goyal <nikiiigoyal@gmail.com>
2025-11-11 19:15:08 +0530

    Add footer

--- src/Store.jsx
+++ src/Store.jsx
@@ -3,7 +3,7 @@ import { proxy } from "valtio";
 
 const state = proxy({
   intro: true, 
-  // array of available colors and logos
+  
   // colors: ['#ccc', '#EFBD4E', '#80C670', '#726DE8', '#EF674E', '#353934'],
   decals: ['binance', 'uniswap', 'ethereum','consensys','solanaLogoMark'],
   // array of selected color and logo
